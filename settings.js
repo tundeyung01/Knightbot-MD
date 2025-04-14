a5c25516886e7ac5a9517ecab88fdbb445fcfa49
tundeyung01 <victorogunwole3@gmail.com>
2025-04-14 01:46:17 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
   packname: 'Knight',
   author: 'Bot',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  botName: "Tunde deadly bot",
+  botOwner: 'Tunde x', // Your name
+  ownerNumber: '2349161868565', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
